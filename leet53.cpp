4f63ce7aecf7ef62332952cc91d7a09b1cc4d8c3
Shreyas HS <shreyashs242006@gmail.com>
2025-11-27 05:29:46 +0530

    Leet code proublem 53 : Maximum sub array sum is added

---
+++ leet53.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum=0;
+        int maxSum=INT_MIN;
+        for(auto val : nums){
+            currentSum+=val;
+            maxSum=max(maxSum,currentSum);
+            if(currentSum<0){
+                currentSum=0;
+            }
+        }
+        return  maxSum;
+        
+    }
+};
+int main()
+{
+    cout<<"Max subarray sum "<<endl;
+    Solution s1;
+    vector<int> nums ={1,-4,7,5,-8};
+    int ans=s1.maxSubArray(nums);
+    cout<<"Maximum sub array sum = "<<ans<<endl;
+    return 0;
+}
