67e14a0814cbb04d26cd466d3c14432a4f9b6788
Shreyas HS <shreyashs242006@gmail.com>
2025-11-27 05:22:56 +0530

    leet136 made change

--- leet136.cpp
+++ leet136.cpp
@@ -13,8 +13,10 @@ public:
     }
 };
 int main(){
+    cout<<"single number in a vector "<<endl;
     Solution s1;
     vector<int>nums={10,20,30,20,30};
     cout<<s1.singleNumber(nums)<<endl;
+    
     return 0;
     }
