ef7580abf3d81696c718aa7ee7170f7641f3d9ce
Shreyas HS <shreyashs242006@gmail.com>
2025-11-28 23:26:46 +0530

    Running sum of a vector is calculated in this question

---
+++ leet1480.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int sum=0;
+        for(int i=1 ; i < nums.size() ; i++){
+            nums[i] += nums[i-1];
+        }
+        return nums ;
+    }
+};
+int main()
+{
+    Solution s1;
+    vector<int>num={1,2,3,4,5,6};
+    vector<int> ans = s1.runningSum(num);
+    for(auto x : ans){
+        cout<<x<<" ";
+    }
+    return 0;
+}
