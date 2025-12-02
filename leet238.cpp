d3ca4ca85529b20aa188c96f1c1d681a2ba262b3
Shreyas HS <shreyashs242006@gmail.com>
2025-12-02 22:20:50 +0530

    Leet code 238 added sucesfully

--- leet238.cpp
+++ leet238.cpp
@@ -5,13 +5,33 @@ using namespace std;
 class Solution{
     public:
     vector<int> productOfArray(vector<int>& nums){
-
+        int n=nums.size();
+        vector<int>answer(n,1);
+        vector<int>prefix(n,1);
+        vector<int>suffix(n,1);
+        //prefix
+        for(int i=1 ; i<n ;i++){
+            prefix[i]=prefix[i-1]*nums[i-1];
+        }
+        //suffix
+        for(int i=n-2 ; i>=0 ;i--){
+            suffix[i]=suffix[i+1]*nums[i+1];
+        }
+        //prefix
+        for(int i=0 ; i<n ;i++){
+            answer[i]=prefix[i]*suffix[i];
+        }
+        return answer;
     }
 
 };
-int main()
-{
-    Solution s1;
 
+int main(){
+    Solution s1;
+    vector<int>nums={1,2,3,4};
+    vector<int>ans = s1.productOfArray(nums);
+    for(auto x:ans){
+    cout<<x<<" ";
+    }
     return 0;
 }
