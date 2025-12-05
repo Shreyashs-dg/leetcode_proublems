6386012b0c4baa8a86ee02f63304e4ab4a6b32ba
Shreyas HS <shreyashs242006@gmail.com>
2025-12-05 21:44:19 +0530

    leet1312 question solved

---
+++ leet1342.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int steps = 0;
+        while(num > 0){
+            if(num % 2 == 0) num /= 2;
+            else num -= 1;
+            steps++;
+        }
+        return steps;
+    }
+};
+
+int main()
+{
+    Solution s1;
+    int num=10;
+    cout<<s1.numberOfSteps(num)<<endl;
+    return 0;
+}
